59f3293b7d86943d0fbf5a0838ecb0d2fce1072e
Satomi Nea <thexamlomaximal@gmail.com>
2024-05-05 13:22:38 +0300

    Split render by two renderers (model + UI)
    and remove debug logs ofc!

--- src/main/java/com/xamlo/core/engine/graphics/components/DefaultSceneRenderer.java
+++ src/main/java/com/xamlo/core/engine/graphics/components/DefaultSceneRenderer.java
@@ -7,8 +7,6 @@ import static org.lwjgl.opengl.GL11.glDrawElements;
 
 import static org.lwjgl.opengl.GL13.GL_TEXTURE0;
 
-import org.joml.Matrix4f;
-import org.joml.Vector3f;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL13;
 
@@ -16,8 +14,7 @@ import com.xamlo.core.engine.graphics.api.components.IScene;
 import com.xamlo.core.engine.graphics.api.components.ISceneRenderer;
 import com.xamlo.core.engine.graphics.api.gui.AbstractSceneElement;
 import com.xamlo.core.engine.graphics.api.gui.AbstractUIElement;
-import com.xamlo.core.engine.graphics.api.gui.IWidget;
-import com.xamlo.core.engine.graphics.components.gui.WidgetGeometry;
+import com.xamlo.core.engine.graphics.api.gui.IUIElement;
 import com.xamlo.core.engine.graphics.opengl.blend.EnumOpenglBlendMode;
 import com.xamlo.core.engine.graphics.opengl.blend.OpenGLBlend;
 import com.xamlo.core.engine.graphics.opengl.depth.EnumOpenGLDepthMode;
@@ -30,9 +27,10 @@ public class DefaultSceneRenderer implements ISceneRenderer {
 	AbstractUIElement debugUIElement = new AbstractUIElement() {
 		//NO-OP DEBUG
 	};
+	private UIElementRenderer uiRenderer;
 	
 	public DefaultSceneRenderer(IResourceLoader<String> resourceLoader) {
-		
+		this.uiRenderer = new UIElementRenderer();
 	}
 
 	@Override
@@ -67,7 +65,8 @@ public class DefaultSceneRenderer implements ISceneRenderer {
 	@Override
 	public void render(IScene scene) {
         
-		System.out.println("i render " + scene.getRenderableObject().size() + " objects");
+		
+		// System.out.println("i render " + scene.getRenderableObject().size() + " objects");
 
 		for (AbstractSceneElement obj : scene.getRenderableObject()) {
 			
@@ -110,144 +109,11 @@ public class DefaultSceneRenderer implements ISceneRenderer {
 		}
 		
 		
-		System.out.println("i render " + scene.getGuiElements().size() + " ui elements");
+		// System.out.println("i render " + scene.getGuiElements().size() + " ui elements");
 
-		for (IWidget obj : scene.getGuiElements()) {
+		for (IUIElement element : scene.getGuiElements()) {
 			
-			WidgetGeometry geometry = obj.getWidgetGeometry();
-			System.out.println("																				");
-			System.out.println("rendering UIElement :" + obj);
-			
-			final float screenWidght = 1920.0f *1;
-			final float screenheight = 1080.0f *1;
-			
-			//Координты начала отрисовки объекта
-			float localScreenXCoord = (((float) geometry.getXCoord() - screenWidght/2)) / (screenWidght); 		// 0,052083332 --- 0,229166662
-			float localScreenYCoord = (((float) geometry.getYCoord() - screenheight/2)) / screenheight;   		// 0,037037037 --- 0,962962937
-			
-			
-
-
-			float displayedWidth = geometry.getWidth()  / screenWidght;							//0,17708333
-			float displayedHeight= geometry.getHeight() / screenheight;							//0,9259259
-
-			
-			Matrix4f positionMatrix = new Matrix4f().identity().translate(localScreenXCoord, localScreenYCoord, 0.666f);
-			
-			if (scene.getGuiElements().indexOf(obj) == 0) {
-				
-				System.out.println("UIElement wight" + this + ". coords:  width [" + geometry.getXCoord() + ", " + (geometry.getWidth() + geometry.getXCoord()) + "]" );
-				System.out.println("UIElement height" + this + ". coords:  height [" + geometry.getYCoord() + ", " + (geometry.getHeight() + geometry.getYCoord()) + "]" );
-
-				System.out.println("UIElement draw X position:  [" + localScreenXCoord + "]" );		// 0,052083332 --- 0,229166662
-				System.out.println("UIElement draw Y position:  [" + localScreenYCoord + "]" );		// 0,037037037 --- 0,962962937
-				
-				System.out.println("UIElement draw width:  [" + displayedWidth  + "]" );			//0.17708333
-				System.out.println("UIElement draw height: [" + displayedHeight + "]" );			//0.9259259
-				
-				Vector3f startPoint = new Vector3f(geometry.getXCoord(), geometry.getYCoord(), 1.0f);
-				System.out.println("vector moved from : [" + startPoint.toString() + "]" );	
-				System.out.println("vector moved to : [" + positionMatrix.transformPosition(startPoint).toString() + "]" );	
-			}
-
-			
-			debugUIElement.getShader().bind();
-			
-			debugUIElement.getShader().setUniform("projectionMatrix", scene.getProjectionMatrix());
-
-			debugUIElement.getShader().setUniform("positionMatrix", positionMatrix);
-
-			/**
-			 * {1.0, 0.0, 0.0,  0.0}
-			 * {0.0, 1.0, 0.0,  0.0}
-			 * {0.0, 0.0, 1.0,  0.0}
-			 * {  X,   Y,   Z,  1.0}
-			 * 
-			 */
-			//Хотя должно быть
-			/**
-			 * {1.0, 0.0, 0.0,    X}
-			 * {0.0, 1.0, 0.0,    Y}
-			 * {0.0, 0.0, 1.0,    Z}
-			 * {0.0, 0.0, 0.0,  1.0}
-			 * 
-			 */
-			Matrix4f rotationMatrix = new Matrix4f().identity().rotateX(0.0f).rotateY(0.0f).rotateZ(0.0f);
-			debugUIElement.getShader().setUniform("rotationMatrix", rotationMatrix);
-
-			/**
-			 * 			    X						Y 								Z
-			 * {1.0,  0.0, 	   0.0,    0.0} * {  cos(y), 0.0, sin(y),  0.0} * {cos(z), -sin(z), 0.0,  0.0}
-			 * {0.0, cos(x), -cos(x),  0.0} * {   0.0,   1.0,  0.0,    0.0} * {sin(z),  cos(z), 0.0,  0.0}
-			 * {0.0, sin(x),  cos(x),  0.0} * { -sin(y), 0.0, сos(y),  0.0} * { 0.0,     0.0,   1.0,  0.0}
-			 * {0.0,  0.0,     0.0,    1.0} * {   0.0,   0.0,  0.0,    1.0} * { 0.0,     0.0,   0.0,  1.0} 
-			 * 
-			 */
-
-			
-			
-			Matrix4f scaleMatrix = new Matrix4f().identity().scale(new Vector3f(displayedWidth, displayedHeight, 1.0f));
-			debugUIElement.getShader().setUniform("scaleMatrix", scaleMatrix);
-
-			/**
-			 * { X , 0.0, 0.0,  0.0}
-			 * {0.0,  Y , 0.0,  0.0}
-			 * {0.0, 0.0,  Z ,  0.0}
-			 * {0.0, 0.0, 0.0,  1.0}
-			 * 
-			 */
-			
-			if (scene.getGuiElements().indexOf(obj) == 0) {
-
-				System.out.println("[MATRIX] updating world matrix for positionMatrix");
-				System.out.println("[MATRIX] {" + positionMatrix.m00() + ", " + positionMatrix.m01() + ", " + positionMatrix.m02() + ", " + positionMatrix.m03() + "}");
-				System.out.println("[MATRIX] {" + positionMatrix.m10() + ", " + positionMatrix.m11() + ", " + positionMatrix.m12() + ", " + positionMatrix.m13() + "}");
-				System.out.println("[MATRIX] {" + positionMatrix.m20() + ", " + positionMatrix.m21() + ", " + positionMatrix.m22() + ", " + positionMatrix.m23() + "}");
-				System.out.println("[MATRIX] {" + positionMatrix.m30() + ", " + positionMatrix.m31() + ", " + positionMatrix.m32() + ", " + positionMatrix.m33() + "}");
-				System.out.println("																																");
-				System.out.println("																																");
-				System.out.println("																																");
-				
-				System.out.println("[MATRIX] updating world matrix for rotationMatrix");
-				System.out.println("[MATRIX] {" + rotationMatrix.m00() + ", " + rotationMatrix.m01() + ", " + rotationMatrix.m02() + ", " + rotationMatrix.m03() + "}");
-				System.out.println("[MATRIX] {" + rotationMatrix.m10() + ", " + rotationMatrix.m11() + ", " + rotationMatrix.m12() + ", " + rotationMatrix.m13() + "}");
-				System.out.println("[MATRIX] {" + rotationMatrix.m20() + ", " + rotationMatrix.m21() + ", " + rotationMatrix.m22() + ", " + rotationMatrix.m23() + "}");
-				System.out.println("[MATRIX] {" + rotationMatrix.m30() + ", " + rotationMatrix.m31() + ", " + rotationMatrix.m32() + ", " + rotationMatrix.m33() + "}");
-				System.out.println("																																");
-				System.out.println("																																");
-				System.out.println("																																");
-
-				System.out.println("[MATRIX] updating world matrix for scaleMatrix");
-				System.out.println("[MATRIX] {" + scaleMatrix.m00() + ", " + scaleMatrix.m01() + ", " + scaleMatrix.m02() + ", " + scaleMatrix.m03() + "}");
-				System.out.println("[MATRIX] {" + scaleMatrix.m10() + ", " + scaleMatrix.m11() + ", " + scaleMatrix.m12() + ", " + scaleMatrix.m13() + "}");
-				System.out.println("[MATRIX] {" + scaleMatrix.m20() + ", " + scaleMatrix.m21() + ", " + scaleMatrix.m22() + ", " + scaleMatrix.m23() + "}");
-				System.out.println("[MATRIX] {" + scaleMatrix.m30() + ", " + scaleMatrix.m31() + ", " + scaleMatrix.m32() + ", " + scaleMatrix.m33() + "}");
-				System.out.println("																													");
-				System.out.println("																													");
-				System.out.println("																													");
-			}
-			
-			debugUIElement.getMesh().bind();
-						
-			if (obj.hasBackgroundImage()) {
-				GL13.glActiveTexture(GL_TEXTURE0);
-				obj.getBackgroundImage().bind();
-			}
-
-			/**
-			 * mode: Задает примитивы для рендеринга, в данном случае треугольники. Здесь никаких изменений.
-			 * count: Указывает количество элементов, которые должны быть отрисованы.
-			 * type: Указывает тип значения в данных индексов. В данном случае мы используем целые числа.
-			 * indices: Задает смещение, которое необходимо применить к данным индексов для начала рендеринга.
-			 */
-			glDrawElements(GL_TRIANGLES, debugUIElement.getMesh().getVertexCount(), GL_UNSIGNED_INT, 0);
-			
-			
-			// На самом деле разбинживать меш вовсе не обязательно, но я так хочу
-			debugUIElement.getMesh().unbind();
-
-			// На самом деле разбинживать шейдер вовсе не обязательно, но я так хочу
-			debugUIElement.getShader().unbind();
+			uiRenderer.renderElement(debugUIElement, element, scene);
 
 		}
 		
---
+++ src/main/java/com/xamlo/core/engine/graphics/components/UIElementRenderer.java
@@ -0,0 +1,82 @@
+package com.xamlo.core.engine.graphics.components;
+
+import static org.lwjgl.opengl.GL11.GL_TRIANGLES;
+import static org.lwjgl.opengl.GL11.GL_UNSIGNED_INT;
+import static org.lwjgl.opengl.GL11.glDrawElements;
+import static org.lwjgl.opengl.GL13.GL_TEXTURE0;
+
+import org.joml.Matrix4f;
+import org.joml.Vector3f;
+import org.lwjgl.opengl.GL13;
+
+import com.xamlo.core.engine.graphics.api.components.IScene;
+import com.xamlo.core.engine.graphics.api.gui.AbstractUIElement;
+import com.xamlo.core.engine.graphics.api.gui.IBackgroundSupport;
+import com.xamlo.core.engine.graphics.api.gui.IResizable;
+import com.xamlo.core.engine.graphics.api.gui.IUIElement;
+import com.xamlo.core.engine.graphics.components.gui.UIElementGeometry;
+
+public class UIElementRenderer {
+
+	
+	public void renderElement(AbstractUIElement debugUIElement, IUIElement element, IScene scene) {
+		
+		if (!(element instanceof IResizable)) {
+			return;
+		}
+		
+		UIElementGeometry geometry = ((IResizable)element).getGeometry();
+	
+		final float screenWidght = 1920.0f *1;
+		final float screenheight = 1080.0f *1;
+		
+		//Координты начала отрисовки объекта
+		float localScreenXCoord = (((float) geometry.getXCoord() - screenWidght/2)) / (screenWidght); 		// 0,052083332 --- 0,229166662
+		float localScreenYCoord = (((float) geometry.getYCoord() - screenheight/2)) / screenheight;   		// 0,037037037 --- 0,962962937
+
+		float displayedWidth = geometry.getWidth()  / screenWidght;							//0,17708333
+		float displayedHeight= geometry.getHeight() / screenheight;							//0,9259259
+
+		
+		Matrix4f positionMatrix = new Matrix4f().identity().translate(localScreenXCoord, localScreenYCoord, 0.666f);
+
+		
+		debugUIElement.getShader().bind();
+		
+		debugUIElement.getShader().setUniform("projectionMatrix", scene.getProjectionMatrix());
+
+		debugUIElement.getShader().setUniform("positionMatrix", positionMatrix);
+
+		Matrix4f rotationMatrix = new Matrix4f().identity().rotateX(0.0f).rotateY(0.0f).rotateZ(0.0f);
+		debugUIElement.getShader().setUniform("rotationMatrix", rotationMatrix);
+		
+		
+		Matrix4f scaleMatrix = new Matrix4f().identity().scale(new Vector3f(displayedWidth, displayedHeight, 1.0f));
+		debugUIElement.getShader().setUniform("scaleMatrix", scaleMatrix);
+
+		debugUIElement.getMesh().bind();
+				
+		if (element instanceof IBackgroundSupport && ((IBackgroundSupport)element).hasBackgroundImage()) {
+			GL13.glActiveTexture(GL_TEXTURE0);
+			((IBackgroundSupport)element).getBackgroundImage().bind();
+		}
+
+		/**
+		 * mode: Задает примитивы для рендеринга, в данном случае треугольники. Здесь никаких изменений.
+		 * count: Указывает количество элементов, которые должны быть отрисованы.
+		 * type: Указывает тип значения в данных индексов. В данном случае мы используем целые числа.
+		 * indices: Задает смещение, которое необходимо применить к данным индексов для начала рендеринга.
+		 */
+		glDrawElements(GL_TRIANGLES, debugUIElement.getMesh().getVertexCount(), GL_UNSIGNED_INT, 0);
+		
+		
+		// На самом деле разбинживать меш вовсе не обязательно, но я так хочу
+		debugUIElement.getMesh().unbind();
+
+		// На самом деле разбинживать шейдер вовсе не обязательно, но я так хочу
+		debugUIElement.getShader().unbind();
+
+		
+	}
+	
+}
